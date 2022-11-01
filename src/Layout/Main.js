57045091baec0fd62bf430ab25c2cc77a7100c4d
YasinSajeeb <yeasinalhossen@gmail.com>
2022-11-01 09:21:29 +0600

    initial commit

---
+++ src/Layout/Main.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Shared/Header/Header';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../Pages/Shared/RIghtSideNav/RightSideNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg="2">
+                    <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col>
+                    <Outlet></Outlet>
+                    </Col>
+                    <Col lg="3">
+                    <RightSideNav></RightSideNav>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAInxrTDe1r7hJ0R-STpUdn-9gYdMz4npk",
+  authDomain: "prime-tech-82655.firebaseapp.com",
+  projectId: "prime-tech-82655",
+  storageBucket: "prime-tech-82655.appspot.com",
+  messagingSenderId: "210547669147",
+  appId: "1:210547669147:web:e6365a43ba210c193b2e3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
